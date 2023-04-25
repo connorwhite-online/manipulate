f820bbe0a81e2266198fa09ea99f7d10c4e3c262
Connor White <connorwhitepdx@gmail.com>
2023-04-25 13:32:56 -0700

    smoothing reduced, comment added

--- src/Model.js
+++ src/Model.js
@@ -7,12 +7,22 @@ export function Model(props) {
   const { nodes, materials } = useGLTF("/caribiner.glb");
   const mesh = useRef();
 
+  // Rotating the model based on 3D landmarks using a smoothing function
   useFrame((state, delta) => {
     if (props.landmarks) {
       const { x, y, z } = props.landmarks;
-      dampE(mesh.current.rotation, [x * 25, y * 100, z * 25], 0.5, delta)
+      dampE(mesh.current.rotation, [x * 25, y * 100, z * 25], 0.25, delta)
     }
   });
+
+  // useFrame(() => {
+  //   if (props.landmarks) {
+  //     const { x, y, z } = props.landmarks;
+  //     mesh.current.rotation.x = x * 25;
+  //     mesh.current.rotation.y = y * 100;
+  //     mesh.current.rotation.z = z * 25;
+  //   }
+  // })
 
   return (
     <group {...props} dispose={null} scale={0.075}>
